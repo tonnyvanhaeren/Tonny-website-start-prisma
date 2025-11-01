43ee0dccabd33a7afb3b9f88612f0b54152ce6ad
Vanhaeren Antonius <antonius.vanhaeren.av@gmail.com>
2025-11-01 17:43:05 +0100

    added profile page - and started with course inscriptions

--- src/routes/auth/profile.tsx
+++ src/routes/auth/profile.tsx
@@ -1,51 +1,78 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import { useAuth } from '../../hooks/useAuth';
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
-  CardFooter,
 } from 'src/components/ui/card';
 import { AtSign, PhoneForwarded, UserStar } from 'lucide-react';
-import { Button } from 'src/components/ui/button';
 
 export const Route = createFileRoute('/auth/profile')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+
   const { isAuthenticated, user, isLoading, isAdmin, hasRole, logout } =
     useAuth();
 
-  if (!user) {
-    //redirect to home
+  if (!isAuthenticated) {
+    navigate({ to: '/', replace: true });
+    return null;
   }
 
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            <h1 className='text-3xl  text-orange-400'>
-              {user?.firstName} - {user?.lastName}
-            </h1>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className='text-lg text-gray-500 flex items-center gap-2'>
-            <AtSign className='w-4 h-4 text-orange-500' />
-            {user?.email}
-          </p>{' '}
-          <p className=' text-gray-500 flex items-center gap-2 text-lg'>
-            <PhoneForwarded className='w-4 h-4 text-green-500' />
-            {user?.phoneNumber}
-          </p>
-          <p className='text-lg text-gray-500 flex items-center gap-2'>
-            <UserStar className='w-4 h-4 text-blue-500' /> {user?.role}
-          </p>
-        </CardContent>
-      </Card>
+      <div className='flex justify-between gap-4'>
+        <div className='max-w-2xl'>
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <h1 className='text-2xl  text-orange-400'>
+                  {user?.firstName} - {user?.lastName}
+                </h1>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className='space-y-2'>
+              <p className='text-md text-gray-500 flex items-center gap-2'>
+                <AtSign className='w-4 h-4 text-orange-500' />
+                {user?.email}
+              </p>{' '}
+              <p className=' text-gray-500 flex items-center gap-2 text-md'>
+                <PhoneForwarded className='w-4 h-4 text-green-500' />
+                {user?.phoneNumber}
+              </p>
+              <p className='text-md text-gray-500 flex items-center gap-2'>
+                <UserStar className='w-4 h-4 text-blue-500' /> {user?.role}
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+        <div className='max-w-5xl min-w-4xl'>
+          <Card>
+            <CardHeader>
+              <CardTitle className='text-2xl  text-orange-400'>
+                Inschrijvingen
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className='flex flex-col gap-2'>
+                <div className='flex items-center gap-2'>
+                  <p className='text-lg text-gray-500'>Inschrijving 1</p>
+                </div>
+                <div className='flex items-center gap-2'>
+                  <p className='text-lg text-gray-500'>Inschrijving 2</p>
+                </div>
+                <div className='flex items-center gap-2'>
+                  <p className='text-lg text-gray-500'>Inschrijving 3</p>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
